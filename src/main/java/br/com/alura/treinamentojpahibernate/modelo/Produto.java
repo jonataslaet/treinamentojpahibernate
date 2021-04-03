d7faae8201a6fcf17bb59e628813f3f0b4ab8820
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-04-03 15:25:25 -0300

    Criado o primeiro produto

--- src/main/java/br/com/alura/treinamentojpahibernate/modelo/Produto.java
+++ src/main/java/br/com/alura/treinamentojpahibernate/modelo/Produto.java
@@ -5,12 +5,14 @@ import java.math.BigDecimal;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "produtos")
 public class Produto {
 
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
---
+++ src/main/java/br/com/alura/treinamentojpahibernate/testes/CadastroDeProduto.java
@@ -0,0 +1,27 @@
+package br.com.alura.treinamentojpahibernate.testes;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.treinamentojpahibernate.modelo.Produto;
+
+public class CadastroDeProduto {
+
+	public static void main(String[] args) {
+		Produto celular = new Produto();
+		celular.setNome("Xiaomi Redmi");
+		celular.setDescricao("Muito legal");
+		celular.setPreco(new BigDecimal("800"));
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("treinamentojpahibernate");
+		EntityManager em = factory.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(celular);
+		em.getTransaction().commit();
+		em.close();
+	}
+}
